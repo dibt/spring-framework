a465b507390931adffa7bf8fb180e0d9743a92a1
povosdi <dibenteng@hzsuidifu.com>
2020-09-14 12:00:22 +0800

    Method.invoke

--- jdk/src/main/java/sun/reflect/NativeMethodAccessorImpl.java
+++ jdk/src/main/java/sun/reflect/NativeMethodAccessorImpl.java
@@ -42,6 +42,8 @@ class NativeMethodAccessorImpl extends MethodAccessorImpl {
 	
 	/**
 	 * 所有的方法反射都是先走 NativeMethodAccessorImpl，默认调了15次之后，才生成一个 GeneratedMethodAccessorXXX 类,
+	 * GeneratedMethodAccessorXXX 类是由  new MethodAccessorGenerator().generateMethod() 生成的
+	 * GeneratedMethodAccessorXXX.invoke 实际上就是直接调用目标对象的具体方法
 	 * 生成好之后就会走这个生成的类的 invoke 方法了
 	 */
     public Object invoke(Object obj, Object[] args)
@@ -53,7 +55,14 @@ class NativeMethodAccessorImpl extends MethodAccessorImpl {
 		// inflationThreshold 默认值是 15
         if (++numInvocations > ReflectionFactory.inflationThreshold()
                 && !ReflectUtil.isVMAnonymousClass(method.getDeclaringClass())) {
-        	// 调用 15 次之后这里生成相关的代理类 GeneratedMethodAccessorXXX
+			/**
+			 * 调用 15 次之后这里生成相关的代理类 GeneratedMethodAccessorXXX
+			 * GeneratedMethodAccessorXXX 的类加载器 DelegatingClassLoader，每次都会生成新的 ClassLoader
+			 * 之所以搞一个新的类加载器，是为了性能考虑，在某些情况下可以卸载这些生成的类，因为类的卸载是只有在类加载器可以被回收的情况下
+			 * 才会被回收的，如果用了原来的类加载器，那可能导致这些新创建的类一直无法被卸载，从其设计来看本身就不希望他们一直存在内存里的，
+			 * 在需要的时候有就行了，在内存紧俏的时候可以释放掉内存
+			 */
+
             MethodAccessorImpl acc = (MethodAccessorImpl)
                 new MethodAccessorGenerator().
                     generateMethod(method.getDeclaringClass(),
@@ -65,7 +74,7 @@ class NativeMethodAccessorImpl extends MethodAccessorImpl {
             // 生成好之后 set 到 DelegatingMethodAccessorImpl
             parent.setDelegate(acc);
         }
-        // 前 15 次调用使用的是 native 实现
+
         return invoke0(method, obj, args);
     }
 
